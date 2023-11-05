4833ea59ef1f7e66525f66eb61582fc07db4fd05
spikeyamk <spikeyamk@github.com>
2023-11-05 14:12:04 +0100

    Basic blinky alongside NimBLE subscribe event notify

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,10 @@
+#include <chrono>
+
 #include "ble.hpp"
+#include "util.hpp"
 
 extern "C" void app_main() {
+	Util::Blinky* blinky_instance = Util::Blinky::get_instance();
+	blinky_instance->start(std::chrono::seconds(1));
 	NimBLE::run();
 }
