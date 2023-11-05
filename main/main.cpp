401fbec9f2bd9182c5e311b1057fe43483120177
spikeyamk <spikeyamk@github.com>
2023-11-05 14:31:10 +0100

    DS3231 driver added

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,18 @@
+#include <thread>
 #include <chrono>
+#include <memory>
 
 #include "ble.hpp"
 #include "util.hpp"
+#include "i2c.hpp"
+#include "ds3231.hpp"
 
 extern "C" void app_main() {
 	Util::Blinky* blinky_instance = Util::Blinky::get_instance();
 	blinky_instance->start(std::chrono::seconds(1));
+	I2CBus i2c_bus {};
+	i2c_bus.scan();
+	auto ds3231 = run_ds3231(i2c_bus);
+	std::this_thread::sleep_for(std::chrono::seconds(10));
 	NimBLE::run();
 }
