140542f2af124b806acfc9a57d96876ff8ffba4b
spikeyamk <spikeyamk@github.com>
2023-11-12 14:55:48 +0100

    Adding magic_packets

--- misc/magic_packets/include/magic_packets.hpp
+++ misc/magic_packets/include/magic_packets.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
 
+namespace MagicPackets {
+	extern const std::array<uint8_t, 20> debug_start;
+	extern const std::array<uint8_t, 20> dump_all_registers;
+	extern const std::array<uint8_t, 20> program_all_registers;
+	extern const std::array<uint8_t, 20> control_HB_command;
+	extern const std::array<uint8_t, 20> debug_end;
+}
