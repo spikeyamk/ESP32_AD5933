49b13f114e403fb1939831959e7065d4dc9a00a7
spikeyamk <spikeyamk@github.com>
2023-11-12 15:15:25 +0100

    magic_packets minor changes

--- misc/magic_packets/lib/test.cpp
+++ misc/magic_packets/lib/test.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <cstdint>
 
 #include "magic_packets.hpp"
 #include "test.hpp"
 
-using namespace MagicPackets;
 void test() {
     bool hasDuplicates = false;
 
-    for (std::size_t i = 0; i < magic_packets.size(); ++i) {
-        for (std::size_t j = i + 1; j < magic_packets.size(); ++j) {
-            if (magic_packets[i] == magic_packets[j]) {
+    for(size_t i = 0; i < MagicPackets::magic_packets.size(); ++i) {
+        for(size_t j = i + 1; j < MagicPackets::magic_packets.size(); ++j) {
+            if (MagicPackets::magic_packets[i] == MagicPackets::magic_packets[j]) {
 				std::printf("ERROR: magic_packets[%zu] is equal to magic_packets[%zu]\n", i, j);
                 hasDuplicates = true;
                 break;
