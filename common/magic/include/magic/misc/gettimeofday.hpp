cd65f160cde4e94a423a2155c671184777a6cbc0
spikeyamk <spikeyamk@github.com>
2024-03-30 22:21:56 +0100

    Magic::Commands::Time::UpdateTimeval::now() uses Windows.h boost::posix_time doesn't work

--- common/magic/include/magic/misc/gettimeofday.hpp
+++ common/magic/include/magic/misc/gettimeofday.hpp
@@ -5,37 +5,10 @@
 	#include <sys/time.h>
 	using mytimeval64_t = timeval;
 #else // for Unix and ESP-IDF
-	#ifdef __linux__
-		#include <cstdint>
-		#include <chrono>
-	#elif _WIN32
-		#include <boost/date_time/posix_time/posix_time.hpp>
-	#endif
-
+	#include <cstdint>
 	struct mytimeval64_t {
 		int64_t tv_sec;
 		int32_t tv_usec;
-		#ifdef __linux__
-			static mytimeval64_t now() {
-				const auto now { std::chrono::high_resolution_clock::now() };
-				const auto secs { std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()) };
-				const auto usecs {
-					std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch())
-					- std::chrono::duration_cast<std::chrono::microseconds>(secs)
-				};
-				return mytimeval64_t {
-					.tv_sec = secs.count(),
-					.tv_usec = static_cast<int32_t>(usecs.count())
-				};
-			}
-		#elif _WIN32
-			static mytimeval64_t now() {
-				const auto now { boost::posix_time::microsec_clock::universal_time() };
-				return mytimeval64_t {
-					.tv_sec = boost::posix_time::to_time_t(now),
-					.tv_usec = static_cast<int32_t>(now.time_of_day().fractional_seconds()),
-				};
-			}
-		#endif
+		static mytimeval64_t now();
 	};
 #endif // end of ifdef _MSC_VER
--- common/magic/lib/misc/gettimeofday.cpp
+++ common/magic/lib/misc/gettimeofday.cpp
@@ -1,25 +1,34 @@
-#pragma once
-
-#ifdef _MSC_VER // for stupid Windows
-// https://gist.github.com/ugovaretto/5875385
-/* 
-* Author: Ugo Varetto - ugovaretto@gmail.com
-* This code is distributed under the terms of the Apache Software License version 2.0
-* https://opensource.org/licenses/Apache-2.0
-*/
-#include <time.h>
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#undef WIN32_LEAN_AND_MEAN
-
 #include "magic/misc/gettimeofday.hpp"
 
-int gettimeofday(mytimeval64_t *tv, timezone *tz) {
-	FILETIME ft;
-	uint64_t tmpres = 0;
-	static int tzflag = 0;
+#ifdef __linux__
+	#include <chrono>
+	mytimeval64_t mytimeval64_t::now() {
+		const auto now { std::chrono::high_resolution_clock::now() };
+		const auto secs { std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()) };
+		const auto usecs {
+			std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch())
+			- std::chrono::duration_cast<std::chrono::microseconds>(secs)
+		};
+		return mytimeval64_t {
+			.tv_sec = secs.count(),
+			.tv_usec = static_cast<int32_t>(usecs.count())
+		};
+	}
+#elif _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+	#undef WIN32_LEAN_AND_MEAN
 
-	if(tv != nullptr) {
+	// https://gist.github.com/ugovaretto/5875385
+	/* 
+	* Author: Ugo Varetto - ugovaretto@gmail.com
+	* This code is distributed under the terms of the Apache Software License version 2.0
+	* https://opensource.org/licenses/Apache-2.0
+	*/
+	mytimeval64_t mytimeval64_t::now() {
+		FILETIME ft;
+		uint64_t tmpres = 0;
+		static int tzflag = 0;
 		GetSystemTimeAsFileTime(&ft);
 
 		tmpres |= ft.dwHighDateTime;
@@ -29,25 +38,15 @@ int gettimeofday(mytimeval64_t *tv, timezone *tz) {
 		tmpres /= 10;  /*convert into microseconds*/
 		/*converting file time to unix epoch*/
 		#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-		static constexpr uint64_t DELTA_EPOCH_IN_MICROSECS = 11644473600000000Ui64;
+			static constexpr uint64_t DELTA_EPOCH_IN_MICROSECS = 11644473600000000Ui64;
 		#else
-		static constexpr uint64_t DELTA_EPOCH_IN_MICROSECS = 11644473600000000ULL;
+			static constexpr uint64_t DELTA_EPOCH_IN_MICROSECS = 11644473600000000ULL;
 		#endif
 		tmpres -= DELTA_EPOCH_IN_MICROSECS; 
-		tv->tv_sec = static_cast<int64_t>(tmpres / 1000000UL);
-		tv->tv_usec = static_cast<int64_t>(tmpres % 1000000UL);
-	}
 
-	if(tz != nullptr) {
-		if(tzflag == 0) {
-			_tzset();
-			tzflag++;
-		}
-		#define _CRT_SECURE_NO_WARNINGS
-		tz->tz_minuteswest = _timezone / 60;
-		tz->tz_dsttime = _daylight;
-		#undef _CRT_SECURE_NO_WARNINGS
+		return mytimeval64_t {
+			.tv_sec = static_cast<int64_t>(tmpres / 1000000UL),
+			.tv_usec = static_cast<int32_t>(tmpres % 1000000UL),
+		};
 	}
-	return 0;
-}
-#endif // end of ifdef _MSC_VER
+#endif
