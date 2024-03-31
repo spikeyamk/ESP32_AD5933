e337338c61e457c691b33c47c408b0642925f503
spikeyamk <spikeyamk@github.com>
2024-03-31 01:14:12 +0100

    before removing boost::process, boost::interprocess

--- control_panel/main.cpp
+++ control_panel/main.cpp
@@ -6,11 +6,6 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 int main(int argc, char* argv[]) {
-    for(size_t i = 0; i < 100; i++) {
-        std::cout << boost::posix_time::to_simple_string(boost::posix_time::microsec_clock::universal_time()) << std::endl;
-        const auto test_tmp { boost::posix_time::to_time_t(boost::posix_time::microsec_clock::universal_time()) };
-    }
-
     static const boost::filesystem::path self_path {
         []() {
             std::basic_string<boost::filesystem::path::value_type> ret;
