efdb1e25c678f5238533d00b66cbbfcef76a3b6c
spikeyamk <spikeyamk@github.com>
2024-05-03 22:27:36 +0200

    minor restruct

--- misc/ble_state_machine/include/ble_state_machine.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "state_machine/state.hpp"
-#include "state_machine/state_derived.hpp"
-#include "state_machine/state_machine.hpp"
-#include "ble_states.hpp"
-
-extern StateMachine::StateMachine<States::bState, decltype(MagicPackets::Debug::Command::start)*> state_machine;
--- misc/sml_sm/include/foo.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "state_machine.hpp"
-
-void fun_foo(NimBLE::T_StateMachine &sm);
-void run_foo();
